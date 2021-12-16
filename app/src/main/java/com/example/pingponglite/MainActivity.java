b2d3497db31dc53b065df47cea46c238a44db280
Anastasia Hristian <an.hristian@gmail.com>
2021-12-16 13:27:12 -0700

    Initial commit

---
+++ app/src/main/java/com/example/pingponglite/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.pingponglite;
+
+import android.view.View;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void startGame(View view) {
+    }
+
+    public void audioPref(View view) {
+    }
+}
